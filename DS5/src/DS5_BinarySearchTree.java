541b95a28d80b4f9263cb5e3ca347c35a1e0bfad
AaronZhenger <alock8mini@gmail.com>
2025-11-20 09:31:46 -0600

    finished BST

--- DS5/src/DS5_BinarySearchTree.java
+++ DS5/src/DS5_BinarySearchTree.java
@@ -75,7 +75,23 @@ public class DS5_BinarySearchTree<E extends Comparable<E>> implements DS5_Binary
 
     @Override
     public int nodeDepth(E value) {
-        return 0;
+        if (!contains(value)) return -1;
+        DS5_BinarySearchTree_Node<E> cur = getRoot();
+        int depth = 0;
+        if (root!=null && root.getData().equals(value)) {
+            return depth;
+        }
+        while (cur!=null) {
+            depth++;
+            if (cur.getLeft()!=null && cur.getLeft().getData().equals(value)) {
+                return depth;
+            }
+            else if (cur.getRight()!=null && cur.getRight().getData().equals(value)) {
+                return depth;
+            }
+            else cur = value.compareTo(cur.getData())<0 ? cur.getLeft() : cur.getRight();
+        }
+        return -999999;
     }
 
     @Override
@@ -96,7 +112,7 @@ public class DS5_BinarySearchTree<E extends Comparable<E>> implements DS5_Binary
 
     @Override
     public int size() {
-        if (isEmpty()) return 0;
+        if (root==null) return 0;
         String raw = printTree(root, 0);
         String[] list = raw.split(" +");
         int size = 0;
@@ -122,7 +138,7 @@ public class DS5_BinarySearchTree<E extends Comparable<E>> implements DS5_Binary
             if (cur.getLeft()!=null && cur.getLeft().getData().equals(data)) {
                 return true;
             }
-            else if (cur.getLeft()!=null && cur.getRight().getData().equals(data)) {
+            else if (cur.getRight()!=null && cur.getRight().getData().equals(data)) {
                 return true;
             }
             else cur = data.compareTo(cur.getData())<0 ? cur.getLeft() : cur.getRight();
@@ -160,22 +176,24 @@ public class DS5_BinarySearchTree<E extends Comparable<E>> implements DS5_Binary
     public boolean remove(E data) {
         DS5_BinarySearchTree_Node<E> cur = getRoot();
         if (root!=null && root.getData().equals(data)) {
-            root = null;
+            root = removeProcess(cur);
             return true;
         }
         while (cur!=null) {
             if (cur.getLeft()!=null && cur.getLeft().getData().equals(data)) {
-                cur.setLeft(null);
+                cur.setLeft(removeProcess(cur.getLeft()));
                 return true;
             }
-            else if (cur.getLeft()!=null && cur.getRight().getData().equals(data)) {
-                cur.setRight(null);
+            else if (cur.getRight()!=null && cur.getRight().getData().equals(data)) {
+                cur.setRight(removeProcess(cur.getRight()));
                 return true;
             }
             else cur = data.compareTo(cur.getData())<0 ? cur.getLeft() : cur.getRight();
         }
         return false;
     }
+
+
 
     private String printTree(DS5_BinarySearchTree_Node<E> node, int order) {
         if (node==null) return " ";
@@ -192,10 +210,45 @@ public class DS5_BinarySearchTree<E extends Comparable<E>> implements DS5_Binary
             default -> "This should not happen";
         };
     }
+
     private int ancestors(DS5_BinarySearchTree_Node<E> node, int curAncestors) {
         if (node==null) return curAncestors-1;
         if (node.getLeft()==null&&node.getRight()==null)
             return curAncestors;
         return Math.max(ancestors(node.getLeft(), curAncestors+1), ancestors(node.getRight(), curAncestors+1));
+    }
+
+    private int children(DS5_BinarySearchTree_Node<E> node) {
+        if (node.getLeft()!=null&&node.getRight()!=null)
+            return 2;
+        if (node.getLeft()==null&&node.getRight()==null)
+            return 0;
+        return 1;
+    }
+
+    private DS5_BinarySearchTree_Node<E> removeProcess(DS5_BinarySearchTree_Node<E> cur) {
+        switch (children(cur)) {
+            case 0 : {
+                return null;
+            }
+            case 1 : {
+                return cur.getLeft()!=null ? cur.getLeft() : cur.getRight();
+            }
+            case 2: {
+                DS5_BinarySearchTree_Node<E> minRight = cur.getRight();
+                while (minRight.getLeft()!=null&&minRight.getLeft().getLeft()!=null)
+                    minRight = minRight.getLeft();
+                if (minRight.getLeft()!=null) {
+                    cur.setData(minRight.getLeft().getData());
+                    minRight.setLeft(minRight.getLeft().getRight());
+                } else {
+                    cur.setData(minRight.getData());
+                    cur.setRight(minRight.getRight());
+                }
+                return cur;
+
+            }
+            default: return null;
+        }
     }
 }
