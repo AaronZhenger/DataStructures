8361def35ab7aac0cbbc241c6402654fefdd7912
AaronZhenger <alock8mini@gmail.com>
2026-03-18 10:17:33 -0500

    idek rn fryn me

--- DS9/src/TextEditorFrame.java
+++ DS9/src/TextEditorFrame.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class TextEditorFrame extends JFrame {
     private static JTabbedPane tabs = new JTabbedPane();
@@ -18,11 +21,12 @@ public class TextEditorFrame extends JFrame {
     private static JMenuItem font = new JMenuItem("Font");
     private static JMenuItem replace = new JMenuItem("Replace");
     private static JMenuItem wordCount = new JMenuItem("Word Count");
+    private static ArrayList<JTextArea> arr = new ArrayList<>();
 
     public TextEditorFrame() {
         super("Text Editor");
         setSize(new Dimension(600, 400));
-        setLayout(null);
+//        setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         create.addActionListener(e -> {
@@ -34,6 +38,10 @@ public class TextEditorFrame extends JFrame {
             String title = "Untitled1";
             while (!foundSpot) {
                 foundSpot = true;
+                if (new File("src\\Saves\\"+title+".txt").exists()) {
+                    foundSpot = false;
+                    title = title.substring(0, title.length()-1)+(Integer.parseInt(title.substring(title.length()-1))+1);
+                }
                 for (int i = 0; i < tabs.getTabCount(); i++) {
                     if (tabs.getTitleAt(i).equals(title)) {
                         foundSpot = false;
@@ -41,35 +49,62 @@ public class TextEditorFrame extends JFrame {
                         break;
                     }
                 }
-
             }
-            File f = new File("C:\\Users\\k2420213\\OneDrive - Katy Independent School District\\Github\\DataStructures\\DS9\\src\\Saves\\"+title+".txt");
+            File f = new File("src\\Saves\\"+title+".txt");
             if (!f.exists()) {
-                try {
-                    f.createNewFile();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                try {f.createNewFile();} catch (IOException ex) {throw new RuntimeException(ex);}
             }
+
+            arr.add(text);
 
             tabs.add(title, textScroll);
         });
         file.add(create);
         open.addActionListener(e -> {
-            JFileChooser fc = new JFileChooser("C:\\Users\\k2420213\\OneDrive - Katy Independent School District\\Github\\DataStructures\\DS9\\src\\Saves");
+            JFileChooser fc = new JFileChooser("src\\Saves");
             FileNameExtensionFilter fnef = new FileNameExtensionFilter("(.txt)", "txt");
             fc.setFileFilter(fnef);
             int r = fc.showOpenDialog(this);
             if (r == JFileChooser.APPROVE_OPTION) {
                 File f = fc.getSelectedFile();
-                JTextArea text = new JTextArea();
-                JScrollPane textScroll = new JScrollPane(text);
-                text.setLineWrap(true);
-                textScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-                tabs.add(f.getName(), textScroll);
+                boolean foundSpot = true;
+                for (int i = 0; i < tabs.getTabCount(); i++) {
+                    if ((tabs.getTitleAt(i)+".txt").equals(f.getName())) {
+                        foundSpot = false;
+                        break;
+                    }
+                }
+                if (foundSpot) {
+                    JTextArea text = new JTextArea();
+                    JScrollPane textScroll = new JScrollPane(text);
+                    text.setLineWrap(true);
+                    textScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                    tabs.add(f.getName().substring(0, f.getName().length()-4), textScroll);
+
+                    arr.add(text);
+                }
             }
         });
         file.add(open);
+        saveAs.addActionListener(e -> {
+            JFileChooser fc = new JFileChooser("src\\Saves");
+            FileNameExtensionFilter fnef = new FileNameExtensionFilter("(.txt)", "txt");
+            fc.setFileFilter(fnef);
+            int r = fc.showOpenDialog(this);
+            if (r == JFileChooser.APPROVE_OPTION) {
+                File f = fc.getSelectedFile();
+                    try {
+                        FileWriter fw = new FileWriter(f);
+                        PrintWriter pw = new PrintWriter(fw);
+                        pw.println(arr.get(tabs.getSelectedIndex()).getText());
+                        fw.close();
+                        pw.close();
+                        tabs.setTitleAt(tabs.getSelectedIndex(), f.getName().substring(0, f.getName().length()-4));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
         saveAs.setEnabled(false);
         file.add(saveAs);
         save.setEnabled(false);
