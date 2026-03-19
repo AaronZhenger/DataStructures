f40efb4b99cd07f34e9d2c6ef82e98e675b85597
AaronZhenger <alock8mini@gmail.com>
2026-03-19 18:25:35 -0500

    Finished file options

--- DS9/src/TextEditorFrame.java
+++ DS9/src/TextEditorFrame.java
@@ -88,6 +88,7 @@ public class TextEditorFrame extends JFrame {
                     }
 
                     tabs.add(f.getName().substring(0, f.getName().length()-4), textScroll);
+                    tabs.setSelectedIndex(tabs.getTabCount()-1);
 
                     paths.add(f.getAbsolutePath());
                     arr.add(text);
@@ -130,7 +131,7 @@ public class TextEditorFrame extends JFrame {
                 try {
                     FileWriter fw = new FileWriter(f);
                     PrintWriter pw = new PrintWriter(fw);
-                    pw.println(arr.get(tabs.getSelectedIndex()).getText());
+                    pw.print(arr.get(tabs.getSelectedIndex()).getText());
                     fw.close();
                     pw.close();
                 } catch (IOException ex) {
@@ -145,14 +146,18 @@ public class TextEditorFrame extends JFrame {
                 StringBuilder sb = new StringBuilder();
                 while (fs.hasNextLine()) {
                     Scanner ls = new Scanner(fs.nextLine());
-                    sb.append(ls.nextLine()).append("\n");
+                    sb.append(ls.hasNextLine() ? ls.nextLine() : "").append("\n");
                 }
                 JTextArea jta = arr.get(tabs.getSelectedIndex());
                 if (jta.getText().contentEquals(sb)) {
                     tabs.remove(tabs.getSelectedIndex());
                 } else {
-                    JOptionPane warning = new JOptionPane("Unsaved data will be lost. Are you sure you want to close this file?", JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
-                    warning.createDialog("Warning");
+                    int selection = JOptionPane.showConfirmDialog(this, "Unsaved data will be lost. Are you sure you want to close this file?", "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                    if (selection==0) {
+                        arr.remove(tabs.getSelectedIndex());
+                        paths.remove(tabs.getSelectedIndex());
+                        tabs.remove(tabs.getSelectedIndex());
+                    }
                 }
             } catch (FileNotFoundException ex) {
                 throw new RuntimeException(ex);
@@ -161,7 +166,32 @@ public class TextEditorFrame extends JFrame {
         close.setEnabled(false);
         file.add(close);
         exit.addActionListener(e -> {
-
+            boolean unsaved = false;
+            try {
+                for (int i = 0; i < tabs.getTabCount(); i++) {
+                    Scanner fs = new Scanner(new File(paths.get(tabs.getSelectedIndex())));
+                    StringBuilder sb = new StringBuilder();
+                    while (fs.hasNextLine()) {
+                        Scanner ls = new Scanner(fs.nextLine());
+                        sb.append(ls.hasNextLine() ? ls.nextLine() : "").append("\n");
+                    }
+                    JTextArea jta = arr.get(tabs.getSelectedIndex());
+                    if (jta.getText().contentEquals(sb)) {
+                        unsaved = true;
+                        break;
+                    }
+                }
+                if (!unsaved) {
+                    System.exit(0);
+                } else {
+                    int selection = JOptionPane.showConfirmDialog(this, "There are files that have not been saved, unsaved data will be lost. Are you sure you want to exit?", "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                    if (selection==0) {
+                        System.exit(0);
+                    }
+                }
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
         });
         file.add(exit);
         mb.add(file);
